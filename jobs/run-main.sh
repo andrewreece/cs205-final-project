5a7414b1a261e7f75d75932eae7f1d81e272f7cd
Andrew Reece <reece@g.harvard.edu>
2015-12-10 14:20:30 -0500

    cleanup

--- jobs/run-main.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# start spark consumer of kafka tweet feed
-(/usr/bin/spark-submit --jars /home/hadoop/spark-streaming-kafka-assembly_2.10-1.5.2.jar /home/hadoop/scripts/spark-output.py &)
-
-# start kafka ingestion of twitter streaming API
-(python /home/hadoop/scripts/twitter-in.py &)
-
