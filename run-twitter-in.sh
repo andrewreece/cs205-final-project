e23033326d2c4fc5b2abff9cbcc9bb13bbbf3da6
Andrew Reece <reece@g.harvard.edu>
2015-12-10 17:48:29 -0500

    Clearing conflicts for merge with andrew branch

--- run-twitter-in.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-python /home/hadoop/scripts/twitter-in.py
-
--- start-kafka-server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Run only on master
-if sudo grep isMaster /mnt/var/lib/info/instance.json | sudo grep true;
-then
-#get things started!
-(sudo /home/hadoop/kafka/bin/kafka-server-start.sh /home/hadoop/kafka/config/server.properties &)
-fi
-
